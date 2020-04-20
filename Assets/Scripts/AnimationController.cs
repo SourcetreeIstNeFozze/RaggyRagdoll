a17b01268fa83a88cf16df66895a3bb5ac9a1c53
kala476 <kala476@gmail.com>
2020-04-20 14:10:29 +0200

    First "controlled"  animation tests

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+	public Animator handAnimator;
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		// INDEX FINGER
+
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			handAnimator.Play("indexStraightUP", 1);
+		}
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			handAnimator.Play("indexStraightDOWN", 1);
+		}
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			handAnimator.Play("indexCurvedUP", 1);
+		}
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			handAnimator.Play("indexCurvedDOWN", 1);
+		}
+
+
+		// MIDDLE FINGER
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			handAnimator.Play("middleStraightUP", 2);
+		}
+		if (Input.GetKeyDown(KeyCode.L))
+		{
+			handAnimator.Play("middleStraightDOWN", 2);
+		}
+		if (Input.GetKeyDown(KeyCode.O))
+		{
+			handAnimator.Play("middleCurvedUP", 2);
+		}
+		if (Input.GetKeyDown(KeyCode.K))
+		{
+			handAnimator.Play("middleCurvedDOWN", 2);
+		}
+	}
+}
