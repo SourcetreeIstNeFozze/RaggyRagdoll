4406c9014763212ee9f72d8d9e7596a1d4f8cf24
Sumpfgulasch <sumpfgulasch@users.noreply.github.com>
2020-04-21 15:03:12 +0200

    Hand kann, wenn hingeflogen, wieder aufstehen (Xbox A-Button oder Leertaste)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody rigid;
     public Vector3 restartForce;
+    SpringToFootPos stabilizeScript;
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = this.GetComponent<Rigidbody>();
+        stabilizeScript = this.GetComponent<SpringToFootPos>();
     }
 
     // Update is called once per frame
@@ -21,10 +23,44 @@ public class PlayerController : MonoBehaviour
 
     void ManagePlayerInput()
     {
+        // Stand Up
         if (Input.GetButtonDown("A Button") || Input.GetKeyDown(KeyCode.Space))
         {
-            rigid.AddForce(restartForce);
-            print("input force");
+            StartCoroutine(StandUp());
         }
+    }
+
+    IEnumerator StandUp()
+    {
+        bool standUp = true;
+        bool standUp_stabilize = false;
+
+        // 1) handfläche darf höchstens zur hälfte aufgerichtet sein, force nach oben adden
+        while (standUp)
+        {
+            rigid.AddForce(restartForce);
+            print("phase 1");
+            
+            if (rigid.position.y > (stabilizeScript.connectedAnchorHeight * 0.9f))
+            {
+                standUp = false;
+                standUp_stabilize = true;
+            }
+            yield return new WaitForFixedUpdate();
+        }
+        
+        // 2) handfläche ist mind. zur hälfte aufgerichtet, jetzt velocity bremsen
+        while (standUp_stabilize)
+        {
+            rigid.velocity /= 1.08f;
+            print("phase 2");
+            if (rigid.velocity.magnitude < 0.2f)
+            {
+                standUp_stabilize = false;
+            }
+            yield return new WaitForFixedUpdate();
+        }
+        
+        yield return null;
     }
 }
