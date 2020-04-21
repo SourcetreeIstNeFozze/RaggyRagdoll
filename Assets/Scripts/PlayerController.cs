12ac68cf6c7791c687292ecfdccb82dfdf6daace
Sumpfgulasch <sumpfgulasch@users.noreply.github.com>
2020-04-21 14:20:58 +0200

    Input-Manager mit allen XBox Controller-Achsen & -Buttons hinzugefügt; Hand stabilisiert sich automatisch + kann umfallen + sich wieder aufrichten (noch nicht fertig)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rigid;
+    public Vector3 restartForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManagePlayerInput();
+    }
+
+    void ManagePlayerInput()
+    {
+        if (Input.GetButtonDown("A Button") || Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.AddForce(restartForce);
+            print("input force");
+        }
+    }
+}
---
+++ Assets/Scripts/SpringToFootPos.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringToFootPos : MonoBehaviour
+{
+    public Transform indexFinger, middleFinger;
+    public float connectedAnchorHeight = 6.4f;
+    public float maxHandForce = 1f;
+
+    Vector3 springPos;
+    Rigidbody rigid;
+    SpringJoint springJoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = this.GetComponent<Rigidbody>();
+        springJoint = this.GetComponent<SpringJoint>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get position between finger tips
+        springPos = (indexFinger.position + middleFinger.position) / 2f;
+        springPos.y = connectedAnchorHeight;
+
+        // limit hand-force
+        springPos = Vector3.MoveTowards(this.transform.TransformPoint(springJoint.anchor), springPos, maxHandForce);
+
+        // assign
+        springJoint.connectedAnchor = springPos;
+
+        Debug.DrawLine(Vector3.zero, springJoint.anchor);
+        Debug.DrawLine(this.transform.TransformPoint(springJoint.anchor), springPos, Color.green);
+    }
+}
