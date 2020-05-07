1025f1654ebf465cb3383bdc8d03b3b310970f3d
kala476 <kala476@gmail.com>
2020-05-07 12:46:10 +0200

    extended copy animation script to include configurable joints, simplified the "hing joint" section of it

--- Assets/Scripts/CopyAnimation.cs
+++ Assets/Scripts/CopyAnimation.cs
@@ -4,26 +4,51 @@ using System.Collections;
 public class CopyAnimation : MonoBehaviour
 {
 
-	public HingeJoint hj;
+	public Joint jointToMove;
 	public Transform target;
+
+	[Header("If Hinge Joint")]
 	[Tooltip("Only use one of these values at a time. Toggle invert if the rotation is backwards.")]
-	public bool x, y, z, invert;
+	public bool x;
+	public bool y;
+	public bool z;
+	public bool invert;
+
+	[Header("ic configurable joint")]
+	private Quaternion initialRotation;
+	private Vector3 initialLocalrotation; 
 
 	void Start()
 	{
-
+		initialRotation = this.transform.rotation;
+		initialLocalrotation = this.transform.localEulerAngles;
 	}
 
 	void Update()
 	{
-		if (hj != null)
+		if (jointToMove != null && target != null)
 		{
-			if (x)
+
+			/// use the axis in the joint to rotation azis -.-
+			if (jointToMove is HingeJoint)
 			{
+				HingeJoint hj = (HingeJoint)jointToMove;
 				JointSpring js;
 				js = hj.spring;
 
-				js.targetPosition = target.transform.localEulerAngles.x;
+				if (x)
+				{
+					js.targetPosition = target.transform.localEulerAngles.x;
+				}
+				else if (y)
+				{
+					js.targetPosition = target.transform.localEulerAngles.y;
+				}
+				else if (z)
+				{
+					js.targetPosition = target.transform.localEulerAngles.z;
+				}
+
 				if (js.targetPosition > 180)
 					js.targetPosition = js.targetPosition - 360;
 				if (invert)
@@ -33,34 +58,14 @@ public class CopyAnimation : MonoBehaviour
 
 				hj.spring = js;
 			}
-			else if (y)
+
+			else if (jointToMove is ConfigurableJoint)
 			{
-				JointSpring js;
-				js = hj.spring;
-				js.targetPosition = target.transform.localEulerAngles.y;
-				if (js.targetPosition > 180)
-					js.targetPosition = js.targetPosition - 360;
-				if (invert)
-					js.targetPosition = js.targetPosition * -1;
+				ConfigurableJoint cj = (ConfigurableJoint)jointToMove;
+				cj.targetRotation = Quaternion.Euler(initialLocalrotation - target.localEulerAngles);
 
-				js.targetPosition = Mathf.Clamp(js.targetPosition, hj.limits.min + 5, hj.limits.max - 5);
-
-				hj.spring = js;
 			}
-			else if (z)
-			{
-				JointSpring js;
-				js = hj.spring;
-				js.targetPosition = target.transform.localEulerAngles.z;
-				if (js.targetPosition > 180)
-					js.targetPosition = js.targetPosition - 360;
-				if (invert)
-					js.targetPosition = js.targetPosition * -1;
 
-				js.targetPosition = Mathf.Clamp(js.targetPosition, hj.limits.min + 5, hj.limits.max - 5);
-
-				hj.spring = js;
-			}
 		}
 	}
 }
