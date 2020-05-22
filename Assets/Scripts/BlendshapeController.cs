b9a5cc592aedb0cb0f0c5b41c70a2634c3562e9d
MalteDe <Malte1998@hotmail.de>
2020-05-22 20:08:34 +0200

    Added simple rotationbased Blendshape script Added new scene for testing that -> Blendshape_test
    if you want to see the results in unity play with the script on Zeigefinger_2 & Zeigefinger_1 or look into Sixththhshhthtsh/Art on discord

---
+++ Assets/Scripts/BlendshapeController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendshapeController : MonoBehaviour
+{
+    [Header("Mesh")]
+    public SkinnedMeshRenderer deformedObject;
+
+    public enum rotAxis {
+        X,
+        Y,
+        Z
+    };
+
+    [Header("Constraint")]
+
+    public rotAxis rotationAxis;
+    public int blendShapeIndex;
+
+    [Range(-1, 1)]
+    public float minAngle;
+    [Range(-1, 1)]
+    public float maxAngle;
+
+    [Header ("Debug - Setup")]
+
+    public Quaternion currentRotation;
+
+    void Update()
+    {
+        UpdateBlendshapes();
+    }
+
+    void UpdateBlendshapes()
+    {
+        currentRotation = this.gameObject.transform.localRotation;
+        if(deformedObject != null)
+        {
+            deformedObject.SetBlendShapeWeight(blendShapeIndex, Mathf.InverseLerp(minAngle, maxAngle, this.gameObject.transform.localRotation.x) * 100);
+        }
+    }
+}
+        // spaeter einbauen ! - vielleicht blendshape by name aktivieren?
+
+//public string[] getBlendShapeNames(GameObject obj)
+//{
+//    SkinnedMeshRenderer head = obj.GetComponent<SkinnedMeshRenderer>();
+//    Mesh m = head.sharedMesh;
+//    string[] arr;
+//    arr = new string[m.blendShapeCount];
+//    for (int i = 0; i < m.blendShapeCount; i++)
+//    {
+//        string s = m.GetBlendShapeName(i);
+//        print("Blend Shape: " + i + " " + s); // Blend Shape: 4 FightingLlamaStance
+//        arr[i] = s;
+//    }
+//    return arr;
+//}
+
+
