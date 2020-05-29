8a79a337d598e7c23d6363b46aba70c41fb81f73
kala476 <kala476@gmail.com>
2020-05-29 12:04:56 +0200

    balance project from that internet guy

---
+++ balance ragdoll by Drolestprawn/Assets/cameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform target;            // The position that that camera will be following.
+    public float smoothing = 5f;        // The speed with which the camera will be following.
+
+    Vector3 offset;                     // The initial offset from the target.
+
+    public Transform camtarg;
+
+    void Start()
+    {
+        // Calculate the initial offset.
+        offset = transform.position - target.position;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void FixedUpdate()
+    {
+        // Create a postion the camera is aiming for based on the offset from the target.
+        Vector3 targetCamPos = target.position + offset;
+
+        // Make the camera look at the player
+        transform.LookAt(target.position);
+
+
+        // Smoothly interpolate between the camera's current position and it's target position.
+        transform.position = Vector3.Lerp(transform.position, camtarg.position, smoothing * Time.deltaTime);
+    }
+}
