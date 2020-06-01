bf1c8265ae771f3eb3e6be1a870280239087c08c
MalteDe <Malte1998@hotmail.de>
2020-06-01 15:07:57 +0200

    Blendshape script update

--- Assets/Scripts/BlendshapeController.cs
+++ Assets/Scripts/BlendshapeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,19 +15,53 @@ public class BlendshapeController : MonoBehaviour
         Z
     };
 
+
     [Header("Constraint")]
 
     public rotAxis rotationAxis;
-    public int blendShapeIndex;
 
+    public bool useBlendShapeIndex = false;
+
+    public int blendShapeIndex = 0;
+
+    public string blendShapeName = "ENTER BLENDSHAPE NAME HERE";
+
+    [Tooltip("Minimum Angle of Axis(as a Quaternion)- Angle where Blendshape is 0")]
     [Range(-1, 1)]
-    public float minAngle;
+    public float minAngle = 0;
+
+    [Tooltip("Maximum Angle of Axis(as a Quaternion)- Angle where Blendshape is 1")]
     [Range(-1, 1)]
-    public float maxAngle;
+    public float maxAngle = 1;
+
+
 
     [Header ("Debug - Setup")]
 
     public Quaternion currentRotation;
+
+    public string[] blendShapeNames;
+
+    bool stopUpdating = false;
+
+
+
+    void Awake()
+    {
+        if (!useBlendShapeIndex)
+        {
+            blendShapeNames = getBlendShapeNames(deformedObject);
+            if (blendShapeNames.Contains(blendShapeName))
+            {
+                blendShapeIndex = Array.FindIndex(blendShapeNames, item => item == blendShapeName);
+            }
+            else
+            {
+                Debug.LogWarning("Blendshapename " + blendShapeName + " was not Found or was typed incorrect, check this gameobject : " + this.gameObject.transform.name);
+                stopUpdating = true;
+            }
+        }
+    }
 
     void Update()
     {
@@ -35,27 +71,27 @@ public class BlendshapeController : MonoBehaviour
     void UpdateBlendshapes()
     {
         currentRotation = this.gameObject.transform.localRotation;
-        if(deformedObject != null)
+        if(deformedObject != null && stopUpdating == false)
         {
             deformedObject.SetBlendShapeWeight(blendShapeIndex, Mathf.InverseLerp(minAngle, maxAngle, this.gameObject.transform.localRotation.x) * 100);
         }
     }
-}
-        // spaeter einbauen ! - vielleicht blendshape by name aktivieren?
 
-//public string[] getBlendShapeNames(GameObject obj)
-//{
-//    SkinnedMeshRenderer head = obj.GetComponent<SkinnedMeshRenderer>();
-//    Mesh m = head.sharedMesh;
-//    string[] arr;
-//    arr = new string[m.blendShapeCount];
-//    for (int i = 0; i < m.blendShapeCount; i++)
-//    {
-//        string s = m.GetBlendShapeName(i);
-//        print("Blend Shape: " + i + " " + s); // Blend Shape: 4 FightingLlamaStance
-//        arr[i] = s;
-//    }
-//    return arr;
-//}
+
+    public string[] getBlendShapeNames(SkinnedMeshRenderer _SkMeRe)
+    {
+        Mesh m = _SkMeRe.sharedMesh;
+        string[] arr;
+        arr = new string[m.blendShapeCount];
+        for (int i = 0; i < m.blendShapeCount; i++)
+        {
+            string s = m.GetBlendShapeName(i);
+            arr[i] = s;
+        }
+        return arr;
+    }
+}
+
+
 
 
