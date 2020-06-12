cab491b763fe1738e6300a33e26d01bbd69dcd22
Sumpfgulasch <sumpfgulasch@users.noreply.github.com>
2020-06-12 16:56:38 +0200

    Implemented beschissenes Cinemachine (Malte du machst damit weiter) into scene; verhindert dass die kamera hin und her springt; cinemachine-script modifiziert

---
+++ Assets/Scripts/AverageRotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AverageRotation : MonoBehaviour
+{
+    Cinemachine.CinemachineTargetGroup.Target[] targets;
+    Vector3 averageRotation;
+    float i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targets = this.GetComponent<Cinemachine.CinemachineTargetGroup>().m_Targets;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        averageRotation = new Vector3();
+        i = 0;
+        foreach(Cinemachine.CinemachineTargetGroup.Target target in targets)
+        {
+            averageRotation += target.target.eulerAngles;
+            i++;
+        }
+        averageRotation = averageRotation / i;
+        averageRotation = new Vector3(0, averageRotation.y, 0);
+        print("aveRot: " + averageRotation);
+
+        this.transform.localEulerAngles = averageRotation;
+    }
+}
