ee6ebb4c8e76db06f3a2c800285e3e471ebad5bf
MalteDe <Malte1998@hotmail.de>
2020-07-01 02:39:02 +0200

    Editor script to change hands should now work :)! changed gradient shader

--- Assets/Scripts/Editor/ChangeHand.cs
+++ Assets/Scripts/Editor/ChangeHand.cs
@@ -6,6 +6,7 @@ using System.Linq;
 
 public class ChangeHand : EditorWindow
 {
+    public static GameObject handParent;
     public static GameObject geometry;
     public static GameObject torso;
     public static GameObject middle3;
@@ -19,8 +20,9 @@ public class ChangeHand : EditorWindow
     int capacity;
 
     public static List<GameObject> objectReferenceList;
+    public static List<GameObject> objectPreviousList;
 
-    public static List<GameObject> objectFieldList;
+    public static List<GameObject> objectInputList;
     public static List<string> objectFieldListNames;
 
 
@@ -28,52 +30,124 @@ public class ChangeHand : EditorWindow
     static void OpenWindow()
     {
         ChangeHand window = (ChangeHand)GetWindow(typeof(ChangeHand));
-        window.minSize = new Vector2(200, 200);
+        window.minSize = new Vector2(700, 200);
         window.Show();
 
-        objectFieldList = new List<GameObject>() { geometry, torso, middle3, middle2, middle1, index3, index2, index1 };
-        objectFieldListNames = new List<string>() { "geometry", "torso", "middle3", "middle2", "middle1", "index3", "index2", "index1" };
-        objectReferenceList = objectFieldList;
+        objectInputList = new List<GameObject>() {handParent ,geometry, torso, middle3, middle2, middle1, index3, index2, index1 };
+        objectFieldListNames = new List<string>() {"hand parent", "geometry", "torso", "middle3", "middle2", "middle1", "index3", "index2", "index1" };
+        objectReferenceList= new List<GameObject>();
+        objectPreviousList= new List<GameObject>();
+
+        foreach (GameObject gO in objectInputList) // set the length of the List to the length of objectFieldList
+        {
+            objectReferenceList.Add(null);
+            objectPreviousList.Add(null);
+        }
     }
 
     private void OnGUI()
     {
-        ObjectFieldCreator(objectFieldList, objectFieldListNames);
-        ObjectFieldCreator(objectReferenceList, objectFieldListNames);
+        ObjectFieldCreator(objectInputList, objectReferenceList, objectPreviousList , objectFieldListNames);
+
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button("Set new Inputs")) // ADD ICONDITION SO THAT EVERYTHING HAS TO BE FILLED IN
+        {
+            SetInputHandToReferernce();
+        }
+        if (GUILayout.Button("Delete old Objects") && !objectPreviousList.Contains(null)) // ADD ICONDITION SO THAT EVERYTHING HAS TO BE FILLED IN
+        {
+            DeleteOldObjects();
+        }
     }
 
-    public void ObjectFieldCreator(List<GameObject> _objectFieldList, List<string> _objectFieldNameList)
+    public void ObjectFieldCreator(List<GameObject> _objectInputList, List<GameObject> _objectReferenceList, List<GameObject> _objectPreviousList, List<string> _objectFieldNameList)
     {
-        capacity = _objectFieldList.Count; //get the amount of fields that has to be drawn
+        capacity = _objectInputList.Count; //get the amount of fields that has to be drawn
                                                         //capacity = EditorGUILayout.IntField("Capacity", capacity); //VISUALIZE THE FIELD
         capacity = Mathf.Max(0, capacity);
 
         //add any new fields
-        for (int m = _objectFieldList.Count; m < capacity; m++)
+        for (int m = _objectInputList.Count; m < capacity; m++)
         {
-            _objectFieldList.Add(null);
+            _objectInputList.Add(null);
+            _objectReferenceList.Add(null);
+            _objectPreviousList.Add(null);
         }
             
 
         //remove extra fields
-        for (int m = capacity; m < _objectFieldList.Count; m++)
+        for (int m = capacity; m < _objectInputList.Count; m++)
         {
-            _objectFieldList.RemoveAt(_objectFieldList.Count - 1);
+            _objectInputList.RemoveAt(_objectInputList.Count - 1);
+            _objectReferenceList.RemoveAt(_objectReferenceList.Count - 1);
+            _objectPreviousList.RemoveAt(_objectReferenceList.Count - 1);
         }
 
         //display the field
-        for (int m = 0; m < _objectFieldList.Count; m++)
+        for (int m = 0; m < _objectInputList.Count; m++)
         {
             EditorGUILayout.BeginHorizontal();
-            _objectFieldList[m] = (EditorGUILayout.ObjectField(_objectFieldNameList[m], _objectFieldList[m], typeof(GameObject), true, GUILayout.Width(500))) as GameObject;
+            _objectReferenceList[m] = (EditorGUILayout.ObjectField(_objectFieldNameList[m] + " Reference", _objectReferenceList[m], typeof(GameObject), true, GUILayout.Width(300))) as GameObject;
+            _objectInputList[m] = (EditorGUILayout.ObjectField(_objectFieldNameList[m] + " Input", _objectInputList[m], typeof(GameObject), true, GUILayout.Width(300))) as GameObject;
+            _objectPreviousList[m] = (EditorGUILayout.ObjectField(_objectFieldNameList[m] + " Prev. Assigned", _objectPreviousList[m], typeof(GameObject), true, GUILayout.Width(300))) as GameObject;
+
             EditorGUILayout.EndHorizontal();
         }
     }
 
 
-    Vector3 getPosition(GameObject _gO)
+    void SetPositionToInput(GameObject _reference, GameObject _input)
     {
-        return _gO.transform.position;
+        _reference.transform.position = _input.transform.position;
+    }
+
+    void SetAVGPositionToInput(GameObject _reference, GameObject _input1 , GameObject _input2)
+    {
+        Vector3 avgVector = (_input1.transform.position + _input2.transform.position) / 2;
+        _reference.transform.position = avgVector;
+    }
+
+    void SetObjectAsParent(GameObject _parent, GameObject _child)
+    {
+        _child.transform.SetParent(_parent.transform, true); // sets the parent WHILE maintaining WORLD pos.
+    }
+
+    void DeleteObject(GameObject _objectToDelete)
+    {
+        DestroyImmediate(_objectToDelete);
+    }
+
+    void SetInputHandToReferernce()
+    {
+        Debug.Log("Clicked the button");
+        //                        0          1        2       3        4        5        6       7      8
+        //objectInputList -> {handParent ,geometry, torso, middle3, middle2, middle1, index3, index2, index1 };
+
+        Undo.RegisterFullObjectHierarchyUndo(objectReferenceList[0], "Reference Object Hierachy"); 
+        Undo.RegisterFullObjectHierarchyUndo(objectInputList[0], "Input Object Hierachy");
+
+        SetAVGPositionToInput(objectReferenceList[0], objectInputList[6], objectInputList[3]); // set torso Reference to avg position based on index3 and middle 3
+        SetObjectAsParent(objectReferenceList[2],  objectInputList[2]); // set torso Input as child of Torso Reference
+
+
+        SetObjectAsParent(objectReferenceList[0], objectInputList[1]); // set Geo Input as Child of HandParent
+
+        for (int i = 3; i < 8; i++) // hardcoded !!!! beware
+        {
+            SetPositionToInput(objectReferenceList[i], objectInputList[i]); // middle 3 - 1 ----- index 3 - 1
+            SetObjectAsParent(objectReferenceList[i], objectInputList[i]);
+        }
+
+    }
+
+    void DeleteOldObjects()
+    {
+
+        for (int i = 0; i < objectFieldListNames.Count; i++)
+        {
+            Undo.RecordObject(objectPreviousList[i], "Deleting Objects");
+            DeleteObject(objectPreviousList[i]);
+        }
     }
 
     private void OnInspectorUpdate()
